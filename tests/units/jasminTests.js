196526c765c651849c1b438b640a5cce8679ae4e
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-05 17:16:39 +0400

    comparisons test & remarkup

--- tests/units/jasminTests.js
+++ tests/units/jasminTests.js
@@ -15,18 +15,28 @@ describe("testing describe: ", function(){
         regexpMatch = 'kirill',
         e = {},
         truthy = "Kirill",
-        arr = ["item1", "item2", "item2"];
+        arr = ["item1", "item2", "item2"],
+        pi = 3.1415926,
+        e = 2.78;
 
     it("expects ...", function(){
         expect(b).toBe(undefined);
         expect(c).toBe(null);
+
         expect(d).toEqual(12);
         expect(d).not.toEqual(11);
+
         expect(regexpMatch).toMatch(/^kir.*$/);
+
         expect(e.d).not.toBeDefined();
         expect(e.d).toBeUndefined();
+
         expect(truthy).not.toBeFalsy();
         expect(truthy).toBeTruthy();
+
         expect(arr).toContain("item2");
+
+        expect(e).toBeLessThan(pi);
+        expect(pi).not.toBeLessThan(e);
     });
 });
