0471d0a1adc48739faf97b619fb26e6d1bcc1af2
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-05 14:03:17 +0400

    jasmine: using simple expects ...

--- tests/units/jasminTests.js
+++ tests/units/jasminTests.js
@@ -8,4 +8,11 @@ describe("testing describe: ", function(){
     it("a to be true", function(){
         expect(a).not.toBe(false);
     });
+
+    var b, c = null;
+
+    it("to be null", function(){
+        expect(b).toBe(undefined);
+        expect(c).toBe(null);
+    });
 });
