ee8032b43012644f6cce4f89354ad0fc0f97ddd2
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-05 22:53:14 +0400

    jasmine: creating 'bare' spy

--- tests/units/jasminTests.js
+++ tests/units/jasminTests.js
@@ -285,4 +285,31 @@ describe("Another tracking properties", function() {
 describe('Cresting bare spy', function() {
     "use strict";
 
+    var whatAmI;
+
+    beforeEach(function() {
+        whatAmI = jasmine.createSpy('whatAmI');
+
+        whatAmI("I", "am", "a", "spy");
+    });
+
+    it("is named, which helps in error reporting", function() {
+        expect(whatAmI.and.identity()).toEqual('whatAmI');
+    });
+
+    it("tracks that the spy was called", function() {
+        expect(whatAmI).toHaveBeenCalled();
+    });
+
+    it("tracks its number of calls", function() {
+        expect(whatAmI.calls.count()).toEqual(1);
+    });
+
+    it("tracks all the arguments of its calls", function() {
+        expect(whatAmI).toHaveBeenCalledWith("I", "am", "a", "spy");
+    });
+
+    it("allows access to the most recent call", function() {
+        expect(whatAmI.calls.mostRecent().args[0]).toEqual("I");
+    });
 });
