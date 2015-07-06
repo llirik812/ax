5ab3ae07838e14fd6229bf1fef3058dffa534a3c
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-07 00:50:23 +0400

    angular jasmine: refactored and added some specs

--- tests/units/testAppSpecs.js
+++ tests/units/testAppSpecs.js
@@ -1,14 +1,23 @@
 describe("TestAppCtrl", function(){
     "use strict";
 
-    var scope = {}, ctrl;
-
     beforeEach(module('testApp'));
 
     it('should create `sorting` array with 7 items in there', inject(function($controller){
-        // creating mock scope
-        ctrl = $controller('testAppCtrl', {$scope : scope});
+        var scope = getScope($controller);
 
         expect(scope.sorting.length).toEqual(7);
     }));
+
+    it('filter should be selected name by default', inject(function($controller){
+        var scope = getScope($controller);
+        expect(scope.input1.option).toEqual('name');
+    }));
+
+    function getScope($controller){
+        var scope = {};
+        // creating mock scope
+        var ctrl = $controller('testAppCtrl', {$scope : scope});
+        return scope;
+    }
 });
