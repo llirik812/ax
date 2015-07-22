0b19c1f87504dcdfe2bfe8425028765cfce92d96
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-23 03:05:50 +0400

    angular: try to use $watch and events

--- app/controllers/route/directiveController.js
+++ app/controllers/route/directiveController.js
@@ -21,4 +21,8 @@ angular.module('routeApp').controller('directiveController', ['$scope', '$routeP
     // Данные переменные мы будем использовать для для директивы отображающей время
     $scope.timeString = 'Current time is ';
     $scope.format = 'M/d/yy h:mm:ss a';
+
+    $scope.destroy = function(){
+        $element.find()
+    }
 }]);
--- app/directives/timeDirective.js
+++ app/directives/timeDirective.js
@@ -7,10 +7,26 @@ angular.module('routeApp').directive('time', ['$interval', 'dateFilter', functio
      * @param attr - передаем hash объект, содержащий стрибуты из элемента
      */
     function link(scope, element, attr) {
-        var format,
-            timeoutId;
+        var timeoutId;
 
-        element.text(attr.local + scope.timeString + dateFilter(new Date(), scope.format));
+        function printTime() {
+            element.text(attr.local + scope.timeString + dateFilter(new Date(), scope.format));
+        }
+
+        // функция следит за переменной, но как работает пока не знаю
+        /*scope.$watch(attr.local, function(newValue, oldValue) {
+            console.log("newValue: " + newValue);
+            console.log("oldValue: " + oldValue);
+        });*/
+
+        printTime();
+
+        $interval(printTime, 1000);
+
+        // Также непойму пока как инициировать данный event
+        /*element.on('$destroy', function(){
+            alert('element destroyed');
+        });*/
     }
 
     return {
