df71e8e754619a44c03637fed3e3df65bc755913
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-26 20:18:18 +0400

    angular: directives scope test: transclude added

--- app/controllers/route/directiveController.js
+++ app/controllers/route/directiveController.js
@@ -55,7 +55,8 @@ angular.module('routeApp').controller('directiveController', ['$scope', '$routeP
     ////////////////////////////////// directive scope //////////////////////
 
     // определяем переменную в пространстве имен контроллера
-    $scope.directiveVar1 = "controller value 1";
-    $scope.directiveVar2 = "controller value 2";
+    $scope.directiveVar1 = 'controller value 1';
+    $scope.directiveVar2 = 'controller value 2';
+    $scope.transcludeVar = 'controller value';
 
 }]);
--- app/directives/directiveScopeTestDirective.js
+++ app/directives/directiveScopeTestDirective.js
@@ -6,11 +6,25 @@ angular.module('routeApp').directive('directiveScopeTest', function() {
         /**
          * Протестировал влияние строки ниже на видимость скопа контроллера из шаблона.
          * Если он определен, то пространство имен контроллера перекрывается
+         * эту строчку следует понимать как создание собственного пространства имен директивы
          */
-        /*scope: {},*/
+        //scope: {},
+        /**
+         * как я понимаю если неопределено пространство имен директивы, то
+         * в аргумент scope передается scope контроллера и шаблону виден этот scope,
+         * если определено, то
+         * в аргумент scope передается scope директивы и шаблону виден scope только директивы и не виден
+         * scope контроллера. Scope контроллера и scope
+         * директивы - разные объекты.
+         * @param scope
+         * @param element
+         */
         link: function(scope, element) {
             scope.directiveVar2 = 'directive value 2 (overriden)';
+            // пытаемся переопределить переменную, которая переносится в блок ng-transclude
+            scope.transcludeVar = "directive value (overriden)"
         },
+        transclude: true,
         templateUrl: '/pages/directives/directiveScopeTestTemplate.html'
     }
 });
