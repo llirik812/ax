46ee07ed0c82aa35bc582887f78a07a1721a5c89
Kirill Osokin <Kirill_Osokin@epam.com>
2015-07-28 23:52:47 +0400

    added mousemove events

--- app/directives/draggableDirective.js
+++ app/directives/draggableDirective.js
@@ -1,43 +1,75 @@
-angular.module('routeApp').directive('draggable', function() {
-    "use strict";
-
-    function link(scope, element) {
-        element.css({
-            position: 'fixed',
-            display: 'block',
-            width: '300px',
-            height: '100px',
-            background: 'green',
-            padding: '10px',
-            'text-align': 'center',
-            cursor: 'pointer',
-            color: 'white',
-            'font-weight': 'bold',
-            'box-shadow': '2px 2px 5px 0 rgba(1, 1, 1, 0.5)',
-            'box-sizing': 'border-box'
-        });
-
-        element.on('mousedown', function(event){
-            $(event.currentTarget).css({
-                border: '1px solid red'
-            });
-        });
-
-        element.on('mouseup', function(event){
-            $(event.currentTarget).css({
-                border: '0px'
-            });
-        });
-
-
-
-    }
-
+angular.module('routeApp').directive('draggable', function($document) {
     return {
+        link: function(scope, element, attr) {
+            var startX = 0, startY = 0, x = 0, y = 0;
+
+            element.css({
+                position: 'absolute',
+                display: 'block',
+                width: '300px',
+                height: '150px',
+                background: 'green',
+                padding: '10px',
+                'text-align': 'center',
+                cursor: 'pointer',
+                color: 'white',
+                'font-weight': 'bold',
+                'box-shadow': '2px 2px 5px 0 rgba(1, 1, 1, 0.5)',
+                'box-sizing': 'border-box'
+            });
+
+            element.on('mousedown', function (event) {
+                // Prevent default dragging of selected content
+                event.preventDefault();
+                startX = event.pageX - x;
+                startY = event.pageY - y;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            function mousemove(event) {
+                x = event.pageX - startX;
+                y = event.pageY - startY;
+                $('#pozX').html(x);
+                $('#pozY').html(y);
+                $('#pageX').html(event.pageX);
+                $('#pageY').html(event.pageY);
+                $('#startX').html(startX);
+                $('#startY').html(startY);
+                element.css({
+                    top: y + 'px',
+                    left: x + 'px'
+                });
+            }
+
+            function mouseup() {
+                $document.off('mousemove', mousemove);
+                $document.off('mouseup', mouseup);
+            }
+        },
         restrict: 'A',
         scope: {},
-        link: link,
         transclude: true,
         templateUrl: '/pages/directives/draggableDirectiveTemplate.html'
-    }
+    };
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
