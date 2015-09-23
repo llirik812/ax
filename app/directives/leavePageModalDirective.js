55fb3a16359a1dc1bcc8e97085ed3c45eff86ecf
Kirill Osokin <Kirill_Osokin@epam.com>
2015-09-23 19:30:23 +0300

    fddfdfddf

--- app/directives/leavePageModalDirective.js
+++ app/directives/leavePageModalDirective.js
@@ -1,9 +1,60 @@
-angular.module('routeApp').directive('leavePageModal', function() {
-    "use strict";
+angular.module('routeApp')
+    .directive('leavePageModal', ['$window', /*'featureDetectorService', */function ($window/*, featureDetectorService*/) {
+        'use strict';
+        return {
+            restrict: 'A',
+            scope: {
+                sjBeforeUnloadMessage: '@',
+                sjBeforeUnloadStopRouting: '&',
+                sjBeforeUnloadIf: '='
+            },
+            link: function (scope, element) {
+                if (_.isUndefined(scope.sjBeforeUnloadIf)) {
+                    scope.sjBeforeUnloadIf = false;
+                }
 
-    return {
-        scope : {},
-        restrict : 'E',
-        templateUrl : 'leave-page-modal.html'
-    }
-});
+                var href;
+
+                /* SCOPE VARIABLES & FUNCTIONS */
+
+                scope.showLeavePageModal = false;
+
+                scope.leavePage = function() {
+                    alert(href);
+                };
+
+                scope.stayPage = function() {
+                    alert('exit');
+                };
+
+                /* HELPER FUNCTIONS */
+
+                function areYouSurePrompt() {
+                    if (scope.sjBeforeUnloadIf) {
+                        return scope.sjBeforeUnloadMessage;
+                    }
+                }
+
+                /* EVENT HANDLERS */
+
+                /*if (featureDetectorService.isIosDevice()) {*/
+                    $('a:not([href^="#"])').bind('click', function(event) {
+                        event.preventDefault();
+                        if (scope.sjBeforeUnloadIf) {
+                            href = event.currentTarget.href;
+                            scope.showLeavePageModal = true;
+                            scope.$apply();
+                        }
+                    });
+                /*}*/
+
+                $(window).bind('beforeunload', areYouSurePrompt);
+
+                element.bind('$destroy', function () {
+                    $(window).unbind('beforeunload', areYouSurePrompt);
+                });
+
+            },
+            templateUrl : '/pages/directives/templates/leavePageModalDirectiveTemplate.html'
+        };
+    }]);
